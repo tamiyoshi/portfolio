89f995b86b1e927e2f8456fa471d553246b69d14
Tamiyoshi <145735768+tamiyoshi@users.noreply.github.com>
2023-11-26 14:17:30 +0900

    14:17

--- scripts/gsap.js
+++ scripts/gsap.js
@@ -2,4 +2,5 @@ var tl = gsap.timeline();
 
 tl.to(".bg_inner_item", 3, { stagger: 0.2, x: "100%" })
   .from(".circle_text", 1.5, { opacity: 0, duration: 0.5 }, "-=2.5")
-  .from(".header", 1, { y: -100 }, "-=2");
+  .from(".header", 1, { y: -100 }, "-=2")
+  .from("#page_top", 1, { x: 100 }, "-=2");
